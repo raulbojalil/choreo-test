d032dc97b75d72fe75f4d633526b83822c8806fe
Raul Bojalil <raulbojalil@gmail.com>
2024-06-19 22:04:09 -0600

    Add new endpoints

--- app.mjs
+++ app.mjs
@@ -1,84 +1,105 @@
 import express from "express";
 import cache from "./cache.mjs";
-import { v4 as uuidv4 } from "uuid";
+//import { v4 as uuidv4 } from "uuid";
 
 const app = express();
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// add a book - request body should contain a title, status and an author
-app.post("/reading-list/books", (req, res) => {
-  const { title, author, status } = req.body;
-  const uuid = uuidv4();
-  if (!(status === "read" || status === "to_read" || status === "reading")) {
-    return res.status(400).json({
-      error: "Status is invalid. Accepted statuses: read | to_read | reading",
-    });
-  }
-  if (!title || !author || !status) {
-    return res.status(400).json({ error: "Title, Status or Author is empty" });
-  }
-  const value = { uuid, title, author, status };
+app.post("/reading-list/employees/:uuid", (req, res) => {
+  const body = req.body;
+  const uuid = req.params.uuid;
+
+  const value = { uuid, content: body };
   cache.set(uuid, value, 86400);
-  return res.status(201).json({ uuid, title, author });
+  return res.status(201).json({ uuid, content: body });
 });
 
-// update status of a book by uuid
-app.put("/reading-list/books/:uuid", (req, res) => {
-  const uuid = req.params.uuid;
-  const { status } = req.body;
-  if (!uuid || typeof uuid !== "string") {
-    return res.status(400).json({ error: "missing or invalid UUID" });
-  }
-  if (!cache.has(uuid)) {
-    return res.status(404).json({ error: "UUID does not exist" });
-  }
-  if (!(status === "read" || status === "to_read" || status === "reading")) {
-    return res.status(400).json({
-      error: "Status is invalid. Accepted statuses: read | to_read | reading",
-    });
-  }
-  const value = cache.get(uuid);
-  value.status = status;
-  cache.set(uuid, value);
-  return res.json({ uuid, status });
-});
-
-// get the list of books
-app.get("/reading-list/books", (_, res) => {
-  const keys = cache.keys();
-  const allData = {};
-  for (const key of keys) {
-    allData[key] = cache.get(key);
-  }
-  return res.json(allData);
-});
-
-// get a book by uuid
-app.get("/reading-list/books/:uuid", (req, res) => {
+app.get("/reading-list/employees/:uuid", (req, res) => {
   const uuid = req.params.uuid;
   if (!uuid || typeof uuid !== "string") {
-    return res.status(400).json({ error: "missing or invalid UUID" });
+    return res.status(400).json({ error: "missing or invalid ID" });
   }
   if (!cache.has(uuid)) {
-    return res.status(404).json({ error: "UUID does not exist" });
+    return res.status(404).json({ error: "ID does not exist" });
   }
   const value = cache.get(uuid);
   return res.json(value);
 });
 
+// add a book - request body should contain a title, status and an author
+// app.post("/reading-list/books", (req, res) => {
+//   const { title, author, status } = req.body;
+//   const uuid = uuidv4();
+//   if (!(status === "read" || status === "to_read" || status === "reading")) {
+//     return res.status(400).json({
+//       error: "Status is invalid. Accepted statuses: read | to_read | reading",
+//     });
+//   }
+//   if (!title || !author || !status) {
+//     return res.status(400).json({ error: "Title, Status or Author is empty" });
+//   }
+//   const value = { uuid, title, author, status };
+//   cache.set(uuid, value, 86400);
+//   return res.status(201).json({ uuid, title, author });
+// });
+
+// update status of a book by uuid
+// app.put("/reading-list/books/:uuid", (req, res) => {
+//   const uuid = req.params.uuid;
+//   const { status } = req.body;
+//   if (!uuid || typeof uuid !== "string") {
+//     return res.status(400).json({ error: "missing or invalid UUID" });
+//   }
+//   if (!cache.has(uuid)) {
+//     return res.status(404).json({ error: "UUID does not exist" });
+//   }
+//   if (!(status === "read" || status === "to_read" || status === "reading")) {
+//     return res.status(400).json({
+//       error: "Status is invalid. Accepted statuses: read | to_read | reading",
+//     });
+//   }
+//   const value = cache.get(uuid);
+//   value.status = status;
+//   cache.set(uuid, value);
+//   return res.json({ uuid, status });
+// });
+
+// get the list of books
+// app.get("/reading-list/books", (_, res) => {
+//   const keys = cache.keys();
+//   const allData = {};
+//   for (const key of keys) {
+//     allData[key] = cache.get(key);
+//   }
+//   return res.json(allData);
+// });
+
+// get a book by uuid
+// app.get("/reading-list/books/:uuid", (req, res) => {
+//   const uuid = req.params.uuid;
+//   if (!uuid || typeof uuid !== "string") {
+//     return res.status(400).json({ error: "missing or invalid UUID" });
+//   }
+//   if (!cache.has(uuid)) {
+//     return res.status(404).json({ error: "UUID does not exist" });
+//   }
+//   const value = cache.get(uuid);
+//   return res.json(value);
+// });
+
 // delete a book by uuid
-app.delete("/reading-list/books/:uuid", (req, res) => {
-  const uuid = req.params.uuid;
-  if (!uuid || typeof uuid !== "string") {
-    return res.status(400).json({ error: "missing or invalid UUID" });
-  }
-  if (!cache.has(uuid)) {
-    return res.status(404).json({ error: "UUID does not exist" });
-  }
-  cache.del(uuid);
-  return res.json({ uuid });
-});
+// app.delete("/reading-list/books/:uuid", (req, res) => {
+//   const uuid = req.params.uuid;
+//   if (!uuid || typeof uuid !== "string") {
+//     return res.status(400).json({ error: "missing or invalid UUID" });
+//   }
+//   if (!cache.has(uuid)) {
+//     return res.status(404).json({ error: "UUID does not exist" });
+//   }
+//   cache.del(uuid);
+//   return res.json({ uuid });
+// });
 
 // health check
 app.get("/healthz", (_, res) => {
